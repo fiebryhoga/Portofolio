da836964fbf4db3932d08b079934aa10ba37330a
fiebryhoga <dimasfiebry@gmail.com.com>
2024-06-02 23:54:42 +0700

    Reset Hard Commit and re-add data

---
+++ app/data/project.js
@@ -0,0 +1,118 @@
+const project = [
+  {
+    id: 2,
+    nama: "Nutrition Calculator",
+    gambar: "/assets/image/project/project2.png",
+    deskripsi:
+      "Nutrition Calculator Gen - 1 Using React, Data Stored in Json Format in Frontend, then will be developed in the second version",
+    link: "https://www.canva.com/design/DAF8L73RLk8/GQ9wQtSScqyqRjJtnYWJhA/edit?utm_content=DAF8L73RLk8&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
+    tools: [
+      {
+        id: 1,
+        namaTools: "ReactJS",
+      },
+      {
+        id: 2,
+        namaTools: "Tailwind",
+      },
+      {
+        id: 3,
+        namaTools: "Web-App",
+      },
+      {
+        id: 3,
+        namaTools: "JavaScript",
+      },
+    ],
+  },
+  {
+    id: 4,
+    nama: "Bakso - Cak No",
+    gambar: "/assets/image/project/project4.png",
+    deskripsi: "Online Food Delevery Web With React Js",
+    link: "https://www.canva.com/design/DAF8L73RLk8/GQ9wQtSScqyqRjJtnYWJhA/edit?utm_content=DAF8L73RLk8&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
+    tools: [
+      {
+        id: 1,
+        namaTools: "ReactJS",
+      },
+      {
+        id: 2,
+        namaTools: "Tailwind",
+      },
+      {
+        id: 3,
+        namaTools: "JavaScript",
+      },
+      {
+        id: 5,
+        namaTools: "Delevery-food",
+      },
+    ],
+  },
+
+  {
+    id: 3,
+    nama: "Chat - App",
+    gambar: "/assets/image/project/project3.png",
+    deskripsi: "Chat App with AJAX",
+    link: "https://www.canva.com/design/DAF8L73RLk8/GQ9wQtSScqyqRjJtnYWJhA/edit?utm_content=DAF8L73RLk8&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
+    tools: [
+      {
+        id: 1,
+        namaTools: "php",
+      },
+      {
+        id: 2,
+        namaTools: "tailwind",
+      },
+      {
+        id: 3,
+        namaTools: "UIVerse",
+      },
+      {
+        id: 4,
+        namaTools: "JavaScript",
+      },
+      {
+        id: 5,
+        namaTools: "Real-Time",
+      },
+      {
+        id: 6,
+        namaTools: "Message",
+      },
+    ],
+  },
+  {
+    id: 1,
+    nama: "My First Website - Abilify TV",
+    gambar: "/assets/image/project/project1.png",
+    deskripsi: "my first site using vanilla version html, css, js",
+    link: "https://www.canva.com/design/DAF8L73RLk8/GQ9wQtSScqyqRjJtnYWJhA/edit?utm_content=DAF8L73RLk8&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
+    tools: [
+      {
+        id: 1,
+        namaTools: "Html",
+      },
+      {
+        id: 2,
+        namaTools: "Css",
+      },
+      {
+        id: 3,
+        namaTools: "JavaScript",
+      },
+      {
+        id: 4,
+        namaTools: "Disability",
+      },
+      {
+        id: 5,
+        namaTools: "Inclusive",
+      },
+    ],
+  },
+];
+
+export default project;
